7f8be1f1bb669ecb8820f1f39487e466e5564840
Mohamed Aziz Nabli <aziznabli7790@gmail.com>
2024-04-25 21:50:39 +0100

    almightyPush

---
+++ Assets/_Scripts/MainMenu/MenuButtons.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class MenuButtons : MonoBehaviour
+{
+    public GameObject[] buttons;
+    void Start()
+    {
+        for(var i = 0; i < buttons.Length; i++)
+        {
+            {
+                buttons[i].transform.DOLocalMoveX(300f +i * 150, 1f).SetEase(Ease.OutExpo).SetDelay(i * 0.1f);
+            }
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void disableButtons()
+    {
+        for (var i = 0; i < buttons.Length; i++)
+        {
+            {
+                buttons[i].transform.DOLocalMoveX(-3000f + i * 150, 1f).SetEase(Ease.OutExpo).SetDelay(i * 0.1f);
+            }
+        }
+    }
+}
